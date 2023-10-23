d3808a94bbd329183f754866dbca2b65e42b3b67
Craig <craigtipple81@gmail.com>
2023-10-23 12:12:28 +0100

    Installed browser router and updated relevant files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  
   return (
     <>
-
+      <Header />
+      <NavigationBar />
+      <main>
+        <h1>NC News</h1>
+        
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
