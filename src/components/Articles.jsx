db9be18a6381265089f06292958bdd3dd1f471e6
Craig <craigtipple81@gmail.com>
2023-10-23 22:16:10 +0100

    Created a new ArticleCard component.

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import api from "../api/api";
-import { Link } from "react-router-dom";
+import ArticleCard from "./ArticleCard";
 
 export default function Articles() {
   const [articles, setArticles] = useState([]);
+  const [search, setSearch] = useState();
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -29,11 +30,6 @@ export default function Articles() {
       );
   }, []);
 
-  const linkStyle = {
-    textDecoration: "none",
-    color: 'black'
-  };
-
   if (isLoading) return <p>Loading...</p>;
   if (error)
     return (
@@ -43,32 +39,13 @@ export default function Articles() {
     );
 
   return (
-    <section className="article-display">
-      {articles.map((article) => {
-        return (
-          <article
-            className="article"
-            key={article.article_id}>
-            <h2>
-              <Link style={linkStyle} to={`/articles/${article.article_id}`}>
-                {article.title}
-              </Link>
-            </h2>
-            <p>Written by: {article.author}</p>
-            <p>About: {article.topic}</p>
-            <p>{article.created_at}</p>
-            <p>Comments: {article.comment_count}</p>
-            <p>Votes: {article.votes}</p>
-            <Link to={`/articles/${article.article_id}`}>
-            <img
-              className="article-image"
-              src={article.article_img_url}
-              alt={`A cover picture reflecting the topic of ${article.topic}`}
-            />
-            </Link>
-          </article>
-        );
-      })}
-    </section>
+    <>
+      <section></section>
+      <section className="article-display">
+        {articles.map((article) => {
+          return <ArticleCard key={article.article_id} article={article} />;
+        })}
+      </section>
+    </>
   );
 }
