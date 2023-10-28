f4d165a16cd31f27092c87ae73bbb6d47f17e147
Craig <craigtipple81@gmail.com>
2023-10-28 15:57:23 +0100

    Updated profile picture of hard-coded user

--- src/components/LoggedUser.jsx
+++ src/components/LoggedUser.jsx
@@ -1,13 +1,11 @@
-import profile from "../../assets/profile-pic.webp";
-
 export default function LoggedUser() {
+
   return (
     <div className="logged-user">
-      
       <img
         id="profilepic"
-        src={profile}
-        alt="A portrait of the user"
+        src={'https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141'}
+        alt="A red jelly person; the avatar of jessjelly."
       />
       <h3>Welcome jessjelly!</h3>  
     </div>
